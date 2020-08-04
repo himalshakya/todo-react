c27833dbd2433c1ae09631222e32db325f732796
Himal Shakya <shakyahimal1@gmail.com>
2020-08-03 22:48:42 -0400

    Adding other libraries

---
+++ webpack.common.js
@@ -0,0 +1,30 @@
+module.exports = {
+  entry: {
+    main: './src/index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/i,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(svg|png|jpe?g|gif)$/i,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[hash].[ext]',
+            outputPath: 'assets',
+          },
+        },
+      },
+    ],
+  },
+};
